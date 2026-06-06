f4edf352d6159b6038635abc7b1f32256ad1ed7e
Joy Gichure <gichureshiru@gmail.com>
2026-06-06 10:54:23 +0300

    Configure dynamic Google Analytics 4 integration via services config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,16 @@
       "url": "https://waverontechnologies.co.ke"
     }
     </script>
+    @if(config('services.google.analytics_id'))
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google.analytics_id') }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '{{ config("services.google.analytics_id") }}');
+        </script>
+    @endif
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <style>
